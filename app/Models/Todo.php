9afe3fae55230214f51db81da7f3fee87ba06345
WesleyRock <wesleyroque224@gmail.com>
2025-10-01 11:48:02 -0400

    initial commit: project created, configured and  model craeted

---
+++ app/Models/Todo.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+final class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tiltle',
+        'description',
+        'completed',
+    ];
+
+    protected $cast = [
+        'completed' => boolean,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
