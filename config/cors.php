38e3fbc4117e0d08b1c9ff5804b3538d5a9855ba
WesleyRock <wesleyroque224@gmail.com>
2025-10-01 18:20:45 -0400

    feat: cors created and configured

---
+++ config/cors.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'], // frontend
+    'allowed_headers' => ['*'],
+];
