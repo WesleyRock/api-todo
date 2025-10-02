421239f73c7dae0034d7ba4c31a4b4f06e95addb
WesleyRock <wesleyroque224@gmail.com>
2025-10-02 01:57:50 -0400

    docs: cors corrected and README created

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'login', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // frontend
+    'allowed_origins' => ['http://localhost:5173'],
     'allowed_headers' => ['*'],
+    'supports_credentials' => false,
 ];
